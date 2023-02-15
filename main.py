40f14ec5f39b9c15b77561a2d609206aa0338914
kujiy <kujiy.fs@gmail.com>
2023-02-15 22:39:41 +0900

    add a reference

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+"""
+Split RGB channel sample
 
+Refer to Maximinusjoshus on https://medium.com/featurepreneur/understanding-the-concept-of-channels-in-an-image-6d59d4dafaa9
+"""
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
     import cv2 as cv
@@ -28,5 +31,3 @@ if __name__ == '__main__':
         cv.destroyAllWindows()
 
 
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
